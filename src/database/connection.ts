b1cdd6f6618100c29b9774017db144a94511eb02
Daniel Vinicius <viniccius774@gmail.com>
2022-09-04 22:22:31 -0300

    Alternativas para env

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,9 +3,9 @@ import knex from 'knex';
 export default knex({
     client: 'mysql2',
     connection: {
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        database: process.env.DATABASE_DATABASE,
-        password: process.env.DATABASE_PASSWORD,
+        host: process.env.DATABASE_HOST || '127.0.0.1',
+        user: process.env.DATABASE_USER || 'root',
+        database: process.env.DATABASE_DATABASE || 'medsoft',
+        password: process.env.DATABASE_PASSWORD || '',
     }
 });
