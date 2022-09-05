4c7bfe1682ef10515b0aa44894f1e08ccef15947
Daniel Vinicius <viniccius774@gmail.com>
2022-09-04 22:08:38 -0300

    Controller de proposito

---
+++ src/controllers/proposito.ts
@@ -0,0 +1,61 @@
+import db from '../database/connection';
+import {Request, Response} from 'express';
+
+export interface proposito {
+    nome: string,
+    descricao: string,
+    id_projeto: number,
+}
+export default class PropositoController {
+    async create(req: Request, res: Response) {
+        const proposito: proposito = req.body;
+        try { 
+            const propositoCriado: number = await db('proposito').insert(proposito);
+            return res.status(200).json(propositoCriado);
+
+        } catch(e: any) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async read(req: Request, res: Response) {
+        try {
+            const proposito: Array<proposito> = await db('proposito').select();
+            return res.status(200).json(proposito);
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async readById(req: Request, res: Response) {
+        const {id} = req.params;
+        try {
+            const proposito: Array<proposito> = await db('proposito').select().where('id', id);
+            return res.status(200).json(proposito);
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async update(req: Request, res: Response) {
+        const {id} = req.params;
+        const update: proposito = req.body;
+
+        try {
+            const propositoExiste: Array<proposito> = await db('proposito').select().where('id', id);
+            if(propositoExiste){
+                const propositoAtualizado: number = await db('proposito').update(update).where('id', id);
+                return res.status(200).json(propositoAtualizado);
+            }
+            return res.status(404).json({err: 'proposito não encontrado'});
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async delete(req: Request, res: Response) {
+        const {id} = req.params;
+        try {
+            const proposito: number = await db('proposito').delete().where('id', id);
+            return res.status(200).json(proposito);
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+}
--- src/routes/projeto.ts
+++ src/routes/projeto.ts
@@ -10,6 +10,7 @@ const controller: ProjetoController = new ProjetoController();
 router
     .get('/', controller.read)
     .get('/:id', controller.readById)
+    .get('/:id/proposito', controller.getProposito)
     .post('/', controller.create)
     .put('/:id', controller.update)
     .delete('/:id', controller.delete);
