ab35d00e87b65fcdf71f25340cc1ee0ab7b8aaf8
Daniel Vinicius <viniccius774@gmail.com>
2022-09-05 19:47:45 -0300

    Controller de indicador e primeiros templates

---
+++ src/controllers/indicador.ts
@@ -0,0 +1,72 @@
+import db from '../database/connection';
+import {Request, Response} from 'express';
+
+export interface indicador {
+    nome: string,
+	tipo?: string,
+	demonstra?: string,
+	como_calcular?: string,
+	como_analisar?: string,
+	termo_lexico?: string,
+	nocao_lexico?: string,
+	impacto_lexico?: string,
+	sinonimo_lexico?: string,
+	fonte_lexico?: string,
+	tipo_lexico?: string,
+	formato_lexico?: string,
+	restricao_lexico?: string,
+	id_objetivo: number,
+}
+export default class IndicadorController {
+    async create(req: Request, res: Response) {
+        const indicador: indicador = req.body;
+        try { 
+            const indicadorCriado: number = await db('indicador').insert(indicador);
+            return res.status(200).json(indicadorCriado);
+
+        } catch(e: any) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async read(req: Request, res: Response) {
+        try {
+            const indicador: Array<indicador> = await db('indicador').select();
+            return res.status(200).json(indicador);
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async readById(req: Request, res: Response) {
+        const {id} = req.params;
+        try {
+            const indicador: Array<indicador> = await db('indicador').select().where('id', id);
+            return res.status(200).json(indicador);
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async update(req: Request, res: Response) {
+        const {id} = req.params;
+        const update: indicador = req.body;
+
+        try {
+            const indicadorExiste: Array<indicador> = await db('indicador').select().where('id', id);
+            if(indicadorExiste){
+                const indicadorAtualizado: number = await db('indicador').update(update).where('id', id);
+                return res.status(200).json(indicadorAtualizado);
+            }
+            return res.status(404).json({err: 'indicador não encontrado'});
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+    async delete(req: Request, res: Response) {
+        const {id} = req.params;
+        try {
+            const indicador: number = await db('indicador').delete().where('id', id);
+            return res.status(200).json(indicador);
+        } catch(e) {
+            return res.status(500).json({err: e});
+        }
+    }
+}
--- src/routes/projeto.ts
+++ src/routes/projeto.ts
@@ -1,5 +1,5 @@
 //Chamando o express e o router que ajuda a manipular as rotas
-import {Router} from 'express';
+import {Router, Request, Response, request} from 'express';
 const router = Router();
 
 //Chamando minha classe controller e instanciando um objeto do tipo ProjetoController
@@ -8,12 +8,15 @@ const controller: ProjetoController = new ProjetoController();
 
 //Todas as rotas necessárias pra fazer o crud, chamando seus respectivo métodos do controller
 router
-    .get('/', controller.read)
-    .get('/:id', controller.readById)
-    .get('/:id/proposito', controller.getProposito)
-    .post('/', controller.create)
-    .put('/:id', controller.update)
-    .delete('/:id', controller.delete);
+    .get('/', (req: Request, res: Response)=> {
+        res.redirect('/projeto');
+    })
+    .get('/projeto', controller.read)
+    .get('/projeto/:id', controller.readById)
+    .get('/projeto/:id/proposito', controller.getProposito)
+    .post('/projeto', controller.create)
+    .put('/projeto/:id', controller.update)
+    .delete('/projeto:id', controller.delete);
 
 //Exporto o router pra ser usado no app.js
 export default router;
